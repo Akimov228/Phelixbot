1c860596fdb2804a4388c3d5d5f57909bbcae059
Akimov228 <113120093+Akimov228@users.noreply.github.com>
2022-12-24 23:56:17 +0600

    Fifth Commit

---
+++ Handlers/notifications.py
@@ -0,0 +1,27 @@
+import aioschedule
+from aiogram import types, Dispatcher
+from config import bot
+import asyncio
+
+
+async def get_chat_id(message: types.Message):
+    global chat_id
+    chat_id = message.from_user.id
+    await message.answer("Ok")
+
+
+async def go_to_training():
+    await bot.send_message(chat_id=chat_id, text="Не забудь завтра тренки в 19:00!!!")
+
+
+async def scheduler():
+    aioschedule.every().saturday.at("23:42").do(go_to_training)
+
+    while True:
+        await aioschedule.run_pending()
+        await asyncio.sleep(2)
+
+
+def register_handlers_notification(dp: Dispatcher):
+    dp.register_message_handler(get_chat_id,
+                                lambda word: 'напомни' in word.text)
--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 from aiogram.utils import executor
-from Handlers import client, callback, extra, admin, fsm_admin
+from Handlers import client, callback, extra, admin, fsm_admin, notifications
 from config import dp
 import logging
-
-
+from Database.bot_db import sql_create
+import asyncio
 
 
 client.register_handlers_client(dp)
 callback.register_handlers_callback(dp)
 admin.register_handlers_admin(dp)
 fsm_admin.register_handlers_fsm_anketa(dp)
+notifications.register_handlers_notification(dp)
 
 extra.register_handler_extra(dp)
 
+
+async def on_startup(_):
+    asyncio.create_task(notifications.scheduler())
+    sql_create()
+
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
